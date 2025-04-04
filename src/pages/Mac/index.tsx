bfb528ce1fc38e69d5716eea4c312ac988106ef6
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-04 20:28:42 +0900

    [Feature] Mac 페이지
    - 배경화면 설정
    - 이미지 변경

--- src/pages/Mac/index.tsx
+++ src/pages/Mac/index.tsx
@@ -1,5 +1,7 @@
 function Mac() {
-  return <h1>Mac</h1>;
+  return (
+    <section className="relative w-screen h-screen bg-[url(/images/mac_light.webp)] bg-no-repeat bg-center bg-cover"></section>
+  );
 }
 
 export default Mac;
