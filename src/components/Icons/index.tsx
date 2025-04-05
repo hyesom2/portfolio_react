e8f9efa66c34debcb06cf56e9f10443ac2c45458
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-05 23:37:43 +0900

    [Modify] Icons
    - Icons 컴포넌트 수정

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,10 +1,11 @@
 interface IconsTypes {
   type: 'mac' | 'instagram';
   name: string;
+  className?: string;
 }
 
-function Icons({ type, name }: IconsTypes) {
-  return <span className={`${type} icon-${name}`} aria-hidden="true"></span>;
+function Icons({ type, name, className }: IconsTypes) {
+  return <span className={`${className} ${type} icon-${name}`} aria-hidden="true"></span>;
 }
 
 export default Icons;
