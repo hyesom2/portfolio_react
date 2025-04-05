9d1b13d607e0afe4eb8411203fd0b040a474acb5
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-05 10:39:57 +0900

    [Feature] Mac 페이지
    - Mac 페이지 생성
    - Menubar, Dock 컴포넌트 추가

--- src/pages/Mac/index.tsx
+++ src/pages/Mac/index.tsx
@@ -1,9 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+import Dock from '@/components/Mac/Dock';
 import Menubar from '@/components/Mac/Menubar';
 
 function Mac() {
   return (
     <section className="relative w-screen h-screen bg-[url(/images/mac_light.webp)] bg-no-repeat bg-center bg-cover">
       <Menubar />
+      <Dock />
+      <Outlet />
     </section>
   );
 }
