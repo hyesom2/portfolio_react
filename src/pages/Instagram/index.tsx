9b68914f9fa9db7f1d1590a6b76b1449f683af72
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-05 23:48:10 +0900

    [Feature] instagram
    - instagram 페이지 구현

--- src/pages/Instagram/index.tsx
+++ src/pages/Instagram/index.tsx
@@ -1,11 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
+import Menubar from '@/components/Instagram/Menubar';
+import WindowFrame from '@/layout/WindowFrame';
+
 function Instagram() {
   return (
-    <div>
-      <Outlet />
-      <h1>Instagram</h1>
-    </div>
+    <WindowFrame className="relative flex flex-col h-full md:flex-row">
+      <h1 className="sr-only">Instagram</h1>
+      <aside className="w-full md:w-60">
+        <Menubar />
+      </aside>
+      <main className="flex-1 h-full overflow-hidden">
+        <Outlet />
+      </main>
+    </WindowFrame>
   );
 }
 
