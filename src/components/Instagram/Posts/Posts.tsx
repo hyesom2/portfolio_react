91ddf9899f5e658aa2806c1b8e57249d15334a06
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-06 01:50:14 +0900

    [Feature] Modal
    - Modal 컴포넌트 생성
    - Dummy data로 열고/닫기 동작과 디자인 구현

--- src/components/Instagram/Posts/Posts.tsx
+++ src/components/Instagram/Posts/Posts.tsx
@@ -1,7 +1,10 @@
 import Icons from '@/components/Icons';
 import PORTFOLIO_DATA from '@/data/Portfolio';
+import { useModalStore } from '@/stores/useModalStore';
 
 function Posts() {
+  const { openModal } = useModalStore();
+
   return (
     <ul className="grid grid-cols-3 gap-1 px-5" role="list">
       {PORTFOLIO_DATA.slice()
@@ -9,12 +12,13 @@ function Posts() {
         .map((portfolio) => (
           <li
             key={portfolio.id}
-            className="relative flex justify-center items-center bg-insta_inactive cursor-pointer"
+            className="bg-insta_inactive relative flex cursor-pointer items-center justify-center"
             role="listitem"
             aria-label={`${portfolio.title} 게시물`}
+            onClick={openModal}
           >
-            <figure className="w-full h-full">
-              <img src={portfolio.mainImage} alt="" className="block w-full h-full object-cover" />
+            <figure className="h-full w-full">
+              <img src={portfolio.mainImage} alt="" className="block h-full w-full object-cover" />
               <figcaption className="sr-only">{portfolio.title}</figcaption>
             </figure>
             <Icons type="instagram" name="gallery" className="absolute top-4 right-4 text-white" />
