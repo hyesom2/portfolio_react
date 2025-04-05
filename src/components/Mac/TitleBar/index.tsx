993d09b95bd8e93090a83f752b61cc4819c7aa66
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-05 23:48:32 +0900

    [Modify] TitleBar
    - TitleBar 컴포넌트 스타일 수정

--- src/components/Mac/TitleBar/index.tsx
+++ src/components/Mac/TitleBar/index.tsx
@@ -6,7 +6,7 @@ interface TitleBarTypes {
 
 function TitleBar({ title }: TitleBarTypes) {
   return (
-    <div className="bg-white flex w-full h-7 relative items-center px-2">
+    <div className="absolute top-0 left-0 bg-white flex w-full h-7 items-center px-2 z-50">
       <h1 className="absolute left-1/2 transform -translate-x-1/2 order-1 text-mac_light-font fs-13">
         {title}
       </h1>
