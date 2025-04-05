06b96b952d2b24cd66475a7a040688e49498f84d
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-06 01:48:59 +0900

    [Modify] Mac
    - Mac 페이지 수정
    - 배경 스타일 RootLayout으로 이동

--- src/pages/Mac/index.tsx
+++ src/pages/Mac/index.tsx
@@ -5,10 +5,10 @@ import Menubar from '@/components/Mac/Menubar';
 
 function Mac() {
   return (
-    <section className="relative w-screen h-screen bg-[url(/images/mac_light.webp)] bg-no-repeat bg-center bg-cover">
+    <section className="relative">
       <Menubar />
-      <Dock />
       <Outlet />
+      <Dock />
     </section>
   );
 }
