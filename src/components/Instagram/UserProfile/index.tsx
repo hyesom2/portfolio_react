e0996bca0522ce3bc5ff2945a06001f54e5425f7
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-06 17:34:02 +0900

    [Refactor] UserProfile
    - 모바일 반응형 추가

--- src/components/Instagram/UserProfile/index.tsx
+++ src/components/Instagram/UserProfile/index.tsx
@@ -4,11 +4,11 @@ function UserProfile() {
   return (
     <>
       <h1 className="sr-only">프로필 이미지</h1>
-      <figure className="relative w-[10rem] h-[10rem] inline-block rounded-full overflow-hidden bg-gradient-to-r from-insta_yellow via-insta_orange to-insta_purple">
+      <figure className="from-insta_yellow via-insta_orange to-insta_purple relative inline-block h-28 w-28 overflow-hidden rounded-full bg-gradient-to-r md:h-[10rem] md:w-[10rem]">
         <img
           src={USER.profile_img}
           alt=""
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  w-[9.375rem] h-[9.375rem] rounded-full block p-1 bg-white"
+          className="absolute top-1/2 left-1/2 block h-26 w-26 -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white p-1 md:h-[9.375rem] md:w-[9.375rem]"
         />
         <figcaption className="sr-only">{USER.name}</figcaption>
       </figure>
