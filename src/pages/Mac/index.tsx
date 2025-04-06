934a5fa1ee69493f2fc6a1aa9bb8c89ae4f4f077
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-06 17:33:19 +0900

    [Remove] Mac
    - Modal 코드 삭제 → 컴포넌트 대신 페이지로 수정

--- src/pages/Mac/index.tsx
+++ src/pages/Mac/index.tsx
@@ -1,18 +1,13 @@
 import { Outlet } from 'react-router-dom';
 
-import Modal from '@/components/Instagram/Modal';
 import Dock from '@/components/Mac/Dock';
 import Menubar from '@/components/Mac/Menubar';
-import { useModalStore } from '@/stores/useModalStore';
 
 function Mac() {
-  const { isOpen } = useModalStore();
-
   return (
     <section className="relative">
       <Menubar />
       <Outlet />
-      {isOpen && <Modal />}
       <Dock />
     </section>
   );
