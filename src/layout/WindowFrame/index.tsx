41d527a68b453ecc6bfdac2b939325eed1d929fc
kimHyeonJu <kimhyesom1202@gmail.com>
2025-04-06 17:30:51 +0900

    [Refactor] WindowFrame
    - 모바일 반응형 추가

--- src/layout/WindowFrame/index.tsx
+++ src/layout/WindowFrame/index.tsx
@@ -7,7 +7,7 @@ interface WindowFrameTypes {
 
 function WindowFrame({ children, className }: WindowFrameTypes) {
   return (
-    <section className="flex flex-col fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-mac_light-windowFrame-bg w-3/4 h-3/4 border border-mac_light-border">
+    <section className="bg-mac_light-windowFrame-bg border-mac_light-border fixed top-1/2 left-1/2 z-50 flex h-full w-full -translate-x-1/2 -translate-y-1/2 transform flex-col border md:h-3/4 md:w-3/4">
       <TitleBar title="instagram" />
       <main className={className}>{children}</main>
     </section>
